c73cbe38af6ca63b6556779991004ba6acf9992e
Samuel Almeida <barbosasamuel24@gmail.com>
2022-04-09 18:38:39 -0300

    Application Properties

--- src/main/java/Main/VendasApplication.java
+++ src/main/java/Main/VendasApplication.java
@@ -2,6 +2,7 @@ package Main;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return applicationName;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
-    }
-
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return applicationName;
     }
 }
