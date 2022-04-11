7d02e70c001ac0a35e69bcee69bb26bd68260254
Samuel Almeida <barbosasamuel24@gmail.com>
2022-04-11 11:06:56 -0300

    Customized annotations and multiple environments added

---
+++ src/main/java/Main/Animal.java
@@ -0,0 +1,5 @@
+package Main;
+
+public interface Animal {
+    void fazerBarulho();
+}
--- src/main/java/Main/MinhaConfiguration.java
+++ src/main/java/Main/MinhaConfiguration.java
@@ -1,18 +1,16 @@
 package Main;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+@Development
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
-    }
-
-    @Bean(name = "outraConfiguracao")
-    public String outraConfiguracao() {
-        return "Sistema de Vendas";
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
     }
 }
